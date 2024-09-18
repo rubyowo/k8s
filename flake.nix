e8d46e2c11e60ac512ddbed3baa01b4ed3bf6a3e
rubyowo <rubyowo284@pm.me>
2024-09-18 19:34:10 +0400

    init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "k8s devShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = let
+            custom-kubernetes-helm = with pkgs;
+              wrapHelm kubernetes-helm {
+                plugins = with pkgs.kubernetes-helmPlugins; [
+                  helm-secrets
+                  helm-diff
+                  helm-git
+                ];
+              };
+            custom-helmfile = pkgs.helmfile-wrapped.override {
+              inherit (custom-kubernetes-helm) pluginsDir;
+            };
+          in
+            with pkgs; [
+              kubectl
+              k9s
+              custom-helmfile
+              custom-kubernetes-helm
+              sops
+            ];
+          env = {
+            KUBECONFIG = "/home/rei/kubeconfig/kubeconfig.yaml";
+          };
+        };
+      }
+    );
+}
